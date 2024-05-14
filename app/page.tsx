a6d726b5cd51cbb98fbeb6aba90a330b892242f7
kn9ck <oknackk@gmail.com>
2024-05-13 21:32:42 -0700

    updated design

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,28 @@
 import Image from "next/image";
 import NavBar from "./ui/navbar";
+import SocialLinks from "./ui/social-links";
 import bg from '../public/lata.png';
 import About from "./ui/sections/about";
+import Projects from "./ui/sections/projects";
 
 export default function Home() {
   return (
-    <main>
-    <div className="absolute -z-10 w-full">
+    <div className="min-h-screen mx-auto max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="lg:flex lg:justify-between lg:gap-4">
+        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+          <div>
+            <h1 className="text-4xl font-bold tracking-tight text-black sm:text-5xl">Joseph Felix</h1>
+            <h2 className="mt-3 text-lg font-medium tracking-tight text-black sm:text-xl">Software Developer</h2>
+            <p className="mt-4 max-w-xs leading-normal text-black">Current student at Arizona State University studying Computer Science. Looking to gain more experience and exposure to working in a team environment.</p>
+            <NavBar />
+          </div>
+          <SocialLinks />
+        </header>
+      <main className="pt-24 lg:w-1/2 lg:py-24">
+        <About />
+        <Projects />
+      </main>
+      </div>
     </div>
-      <NavBar/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <About/>
-      <div className="text-4xl text-black" id="lata">lata</div>
-    </main>
   );
 }
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function NavBar(){
-  return <div className="bg-black bg-cover flex justify-between items-center h-24  mx-auto px-4 text-white sticky top-0">
-    <h1 className="w-full text-3xl font-bold">Joseph Felix</h1>
-    <ul className="flex justify-between items-center">
-      <li className="p-4 flex max-h-10 items-center"><Link href="#about-section" scroll={false} >About</Link></li>
-      <li className="p-4 flex max-h-10 items-center"><Link href="#lata" >Skills</Link></li>
-      <li className="p-4 flex max-h-10 items-center"><Link href="#projects-section" >Projects</Link></li>
-      <li className="p-4 flex max-h-10 items-center"><Link href="#education-section" >Education</Link></li>
-    </ul>
-  </div>;
+export default function NavBar() {
+  return <nav className="nav hidden lg:block text-black">
+      <ul className="mt-16 w-max">
+        <li ><Link className="group flex items-center py-3" href="#about" >About</Link></li>
+        <li ><Link className="group flex items-center py-3" href="#projects" >Projects</Link></li>
+        <li ><Link className="group flex items-center py-3" href="#education" >Education</Link></li>
+      </ul>
+  </nav>;
 }
